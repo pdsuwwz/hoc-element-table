1d1ffd5cbba3a9622f08643c6ff87b0059647723
pdsuwwz <pdsu.wwz@foxmail.com>
2022-01-01 11:31:42 +0800

    🌹 feat: update banner

--- example/router.js
+++ example/router.js
@@ -1,19 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const Layout = () => import('example/components/Layout')
 const importModule = (filePath) => {
   return () => import(`example/${filePath}`)
 }
 
 const routes = [{
   path: '/',
-  component: Layout,
-  children: [
-    {
-      path: 'table-example',
-      component: importModule('views/ExampleTable')
-    }
-  ]
+  component: importModule('views/ExampleTable')
 }]
 
 export default createRouter({
--- example/src/components/table-children-a.vue
+++ example/src/components/table-children-a.vue
@@ -2,7 +2,6 @@
   <div>
     <div class="block">
       <span class="demonstration">轮播组件</span>
-      <el-button>233</el-button>
       <el-carousel height="150px">
         <el-carousel-item
           v-for="item in 3"
